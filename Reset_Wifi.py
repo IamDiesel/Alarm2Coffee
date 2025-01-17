61d8e74a4a13e4948a1e0391a30fb02c47df0443
IamDiesel <Daniel.Kahrizi@gmx.de>
2025-01-17 14:25:45 +0100

    Fix Home Assistant WIFI stability issues

--- Reset_Wifi.py
+++ Reset_Wifi.py
@@ -11,19 +11,19 @@ class Wifi_Deamon(Thread):
     def run(self):
         self._running = True
         while(self._running):
-
-            if(self.is_gateway_reachable == False):
+            
+            if(self.is_gateway_reachable() == False):
                 self.reset_WLAN()
                 print("resetting WLAN")
             else:
                 print("Connection OK")
-            time.sleep(60)
+            time.sleep(10)
             
     def is_gateway_reachable(self):
         try:
             reachable = False
             res = self.shell_command(f"ping {self.gateway_ip} -c 1 | grep 'received' | awk -F',' " +"'{ print $2}' | awk '{ print $1}'")
-            #print(res)
+            print(res)
 
             if(int(res) == 1):
                 reachable = True
@@ -36,34 +36,33 @@ class Wifi_Deamon(Thread):
 
     def get_interface(self):
         interface = None
-        res = shell_command("iwgetid")
+        res = self.shell_command("iwgetid")
         if("ESSID" in res):
             interface = res.split("     ")[0]
         return interface
 
     def get_SSID(self):
         interface = None
-        res = shell_command("iwgetid")
+        res = self.shell_command("iwgetid")
         if("ESSID" in res):
             interface = res.split('"')[1]
         return interface
 
     def ip_link_up(self):
-        print(shell_command(f"ip link set {getInterface()} up"))
+        print(self.shell_command(f"sudo ip link set {self.get_interface()} up"))
         
     def ip_link_down(self):
-        print(shell_command(f"ip link set {getInterface()} down"))
+        print(self.shell_command(f"sudo ip link set {self.get_interface()} down"))
 
     def reset_WLAN(self):
-        ipLinkDown()
+        self.ip_link_down()
         time.sleep(10)
-        ipLinkUp()
+        self.ip_link_up()
     
-
-
-wlan_ref = Wifi_Deamon("192.168.2.1")
-wlan_thread = Thread(target = wlan_ref.run)
-wlan_thread.start()
-wlan_thread.join()
+if __name__ == '__main__':
+    wlan_ref = Wifi_Deamon("192.168.2.1")
+    wlan_thread = Thread(target = wlan_ref.run)
+    wlan_thread.start()
+    wlan_thread.join()
 
 
