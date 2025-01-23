9c4e88d227d1183ee2338d739ed86f78e52d844f
IamDiesel <Daniel.Kahrizi@gmx.de>
2025-01-23 21:37:57 +0100

    reset wifi: full device activation

--- Reset_Wifi.py
+++ Reset_Wifi.py
@@ -4,6 +4,7 @@ from threading import Thread
 
 class Wifi_Deamon(Thread):
     def __init__(self, gateway_ip):
+        self._retries = 0
         self.gateway_ip = gateway_ip
         self._running = False
         super(Wifi_Deamon, self).__init__()
@@ -16,20 +17,33 @@ class Wifi_Deamon(Thread):
         while(self._running):
             
             if(self.is_gateway_reachable() == False):
+                self._retries += 1
                 self.reset_WLAN()
                 print("resetting WLAN")
             else:
                 print("Connection OK")
+                self._retries = 0
+            
+            #reboot
+            if(self._retries > 10):
+                self.radio_on()
+                time.sleep(10)
+                self.ip_link_up()
+                time.sleep(10)
+                self.reboot()
+                
             time.sleep(10)
             
     def is_gateway_reachable(self):
         try:
             reachable = False
+            
             res = self.shell_command(f"ping {self.gateway_ip} -c 1 | grep 'received' | awk -F',' " +"'{ print $2}' | awk '{ print $1}'")
             print(res)
 
             if(int(res) == 1):
                 reachable = True
+                
         except Exception:
             pass
         return reachable
@@ -38,7 +52,7 @@ class Wifi_Deamon(Thread):
         return os.popen(input).read()
 
     def get_interface(self):
-        interface = None
+        interface = "wlan0"
         res = self.shell_command("iwgetid")
         if("ESSID" in res):
             interface = res.split("     ")[0]
@@ -54,13 +68,43 @@ class Wifi_Deamon(Thread):
     def ip_link_up(self):
         print(self.shell_command(f"sudo ip link set {self.interface} up"))
         
+    def radio_on(self):
+        print(self.shell_command("sudo nmcli radio wifi on"))
+        
     def ip_link_down(self):
         print(self.shell_command(f"sudo ip link set {self.interface} down"))
+        
+    def is_ip_link_up(self):
+        res = self.shell_command(f"cat /sys/class/net/{self.interface}/carrier")
+        print(res)
+        if(res == "1\n"):
+            return True
+        else:
+            return False
 
+    def radio_off(self):
+        print(self.shell_command("sudo nmcli radio wifi off"))
+        
+    def reboot(self):
+        print("rebooting")
+        print(self.shell_command("sudo reboot"))
+        
     def reset_WLAN(self):
         self.ip_link_down()
-        time.sleep(15)
-        self.ip_link_up()
+        time.sleep(10)
+        self.radio_off()
+        time.sleep(10)
+        while(self.is_ip_link_up() == False):
+            self.radio_on()
+            time.sleep(10)
+            self.ip_link_up()
+            time.sleep(15)
+        time.sleep(60)
+ 
+if __name__ == '__main__1':
+     wlan_ref = Wifi_Deamon("192.168.2.1")
+     print(wlan_ref.is_ip_link_up())
+            
     
 if __name__ == '__main__':
     wlan_ref = Wifi_Deamon("192.168.2.1")
